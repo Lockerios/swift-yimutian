5d5be690d8c97e11efaee4f40de34361749b4eb1
lockerios <locklockzhang@gmail.com>
2015-10-29 22:32:23 +0800

    add collectionView.

--- Yimutian/HomeViewController.swift
+++ Yimutian/HomeViewController.swift
@@ -8,9 +8,12 @@
 
 import UIKit
 
-class HomeViewController: BaseViewController {
+class HomeViewController: BaseViewController, UICollectionViewDataSource, UICollectionViewDelegate ,UICollectionViewDelegateFlowLayout{
 
+    let cellIdentifier = "cellIdentifier"
+    
     var searchBtn: UIBarButtonItem!
+    var collectionView: UICollectionView!
     
     //MARK: - Lifecycle
     
@@ -19,6 +22,15 @@ class HomeViewController: BaseViewController {
         
         searchBtn = UIBarButtonItem(title: "搜索", style: .Plain, target: self, action: "p_searchBtnCalled")
         self.navigationItem.rightBarButtonItem = searchBtn
+        
+        let layout = UICollectionViewFlowLayout()
+        collectionView = UICollectionView(frame: SCREEN_BOUNDS, collectionViewLayout: layout)
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.backgroundColor = UIColor.whiteColor()
+        collectionView.registerClass(UICollectionViewCell.classForCoder(), forCellWithReuseIdentifier: cellIdentifier)
+        
+        self.view.addSubview(collectionView)
     }
     
     override func didReceiveMemoryWarning() {
@@ -42,6 +54,44 @@ class HomeViewController: BaseViewController {
 
     //MARK: - Actions
     
+    //MARK: UICollectionViewDataSource
     
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        return 3
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellIdentifier, forIndexPath: indexPath)
+        
+        switch indexPath.section {
+        case 0:
+            cell.backgroundColor = UIColor.yellowColor()
+        case 1:
+            cell.backgroundColor = UIColor.grayColor()
+        case 2:
+            cell.backgroundColor = UIColor.purpleColor()
+        default:
+            cell.backgroundColor = UIColor.blackColor()
+        }
+        
+        
+        return cell
+    }
+    
+    //MARK: UICollectionViewDelegate
+    
+    func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        dLog("\(indexPath.row) tapped")
+    }
+    
+    //MARK: UICollectionViewDelegateFlowLayout
+    
+    func collectionView(collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
+        return CGSizeMake(collectionView.bounds.size.width, 44)
+    }
 }
 
