e6e71f2c339987559ba3452071ad8285e2bbfddd
lockerios <locklockzhang@gmail.com>
2015-11-02 16:49:29 +0800

    add sharedInstance

---
+++ Yimutian/YMTApi.swift
@@ -0,0 +1,24 @@
+//
+//  YMTApi.swift
+//  Yimutian
+//
+//  Created by yangyang on 15/11/2.
+//  Copyright © 2015年 LLZ. All rights reserved.
+//
+
+import Foundation
+
+private let sharedInstance = YMTApi()
+
+class YMTApi {
+    
+    //MARK: - Lifecycle
+    
+    class var sharedYMTApi: YMTApi {
+        return sharedInstance
+    }
+    
+    //MARK: - Methods
+    
+    
+}
