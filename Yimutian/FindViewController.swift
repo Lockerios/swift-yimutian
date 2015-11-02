ab059d8048021f97f04692f7d12c4ab08199d709
lockerios <locklockzhang@gmail.com>
2015-11-02 10:36:18 +0800

    add seg and tableView.

--- Yimutian/FindViewController.swift
+++ Yimutian/FindViewController.swift
@@ -8,13 +8,41 @@
 
 import UIKit
 
-class FindViewController: BaseViewController {
+class FindViewController: BaseViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    var segmentController: UISegmentedControl!
+    var dataTypePicker: UIPickerView!
+    var pickerBtn: UIButton!
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        segmentController = UISegmentedControl(items: ["找买家", "找卖家"])
+        segmentController.selectedSegmentIndex = 0
+        segmentController.addTarget(self, action: Selector("p_segmentValueChanged"), forControlEvents: .ValueChanged)
+        
+        self.navigationItem.titleView = segmentController
+        
+        dataTypePicker = UIPickerView(frame: CGRectMake(0,SCREEN_HEIGHT-220-60,SCREEN_WIDTH,220))
+        dataTypePicker.dataSource = self
+        dataTypePicker.delegate = self
+        dataTypePicker.hidden = true
+        dataTypePicker.layer.borderColor = UIColor.grayColor().CGColor
+        dataTypePicker.layer.borderWidth = 1.0
+        
+        view.addSubview(dataTypePicker)
+        
+        pickerBtn = UIButton(type: .Custom)
+        pickerBtn.frame = CGRectMake(0, 60, SCREEN_WIDTH, 44)
+        pickerBtn.setTitle("北京市", forState: .Normal)
+        pickerBtn.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        pickerBtn.addTarget(self, action: Selector("p_showPicker"), forControlEvents: .TouchUpInside)
+        pickerBtn.layer.borderColor = UIColor.redColor().CGColor
+        pickerBtn.layer.borderWidth = 1.0
+        
+        view.addSubview(pickerBtn)
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,8 +52,42 @@ class FindViewController: BaseViewController {
     
     //MARK: - Methods
     
+    func p_segmentValueChanged() {
+        dLog("\(segmentController.selectedSegmentIndex)")
+    }
     
+    func p_showPicker() {
+        dLog("show Picker")
+        dataTypePicker.hidden = !dataTypePicker.hidden
+    }
     
     //MARK: - Actions
 
+    //MARK: UIPickerViewDataSource
+    
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 5
+    }
+    
+    //MARK: UIPickerViewDelegate
+    
+    func pickerView(pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+        return SCREEN_WIDTH/3
+    }
+    
+    func pickerView(pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 44
+    }
+    
+    func pickerView(pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(component)"+" "+"\(row)"
+    }
+    
+    func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        dLog("\(component)"+" "+"\(row)"+" selected")
+    }
 }
--- Yimutian/MsgViewController.swift
+++ Yimutian/MsgViewController.swift
@@ -8,13 +8,29 @@
 
 import UIKit
 
-class MsgViewController: BaseViewController {
+class MsgViewController: BaseViewController, UITableViewDataSource, UITableViewDelegate {
 
+    let cellIdentifier = "cellIdentifier"
+    
+    var tableView: UITableView!
+    var refreshControl: UIRefreshControl!
+    
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView = UITableView(frame: SCREEN_BOUNDS)
+        tableView.dataSource = self
+        tableView.delegate = self
+        tableView.registerClass(UITableViewCell.classForCoder(), forCellReuseIdentifier: cellIdentifier)
+        
+        view.addSubview(tableView)
+        
+        refreshControl = UIRefreshControl()
+        refreshControl.attributedTitle = NSAttributedString(string: "Pull to refresh")
+        refreshControl.addTarget(self, action: "p_refresh", forControlEvents: UIControlEvents.ValueChanged)
+        tableView.addSubview(refreshControl)
     }
     
     override func didReceiveMemoryWarning() {
@@ -24,8 +40,36 @@ class MsgViewController: BaseViewController {
     
     //MARK: - Methods
     
+    func p_refresh() {
+        self.performSelector(Selector("p_loadData"), withObject: nil, afterDelay: 1.5)
+    }
     
+    func p_loadData() {
+        refreshControl.endRefreshing()
+    }
     
     //MARK: - Actions
 
+    //MARK: UITableViewDataSource
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier)
+        cell?.textLabel!.text = "未读消息，点击查看"
+        
+        return cell!
+    }
+    
+    //MARK: UITableViewDelegate
+    
+    func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
+        return 100
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        dLog("\(indexPath.row) selected")
+    }
 }
